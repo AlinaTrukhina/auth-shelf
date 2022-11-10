eb93c428c345be4e0a361cad6c279afee9382215
Tormod Sletteboe <tormod.slettebo@gmail.com>
2022-11-10 10:45:54 -0600

    shelfpage.js stuff

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+import { useState, useEffect } from 'react';
+import { useHistory, Link } from 'react-router-dom';
 
 function ShelfPage() {
+
+  const dispatch = useDispatch();
+  const shelf = useSelector((store) => store) //TODO: need to get shelf not store
+
+  useEffect(() => {
+
+    dispatch({
+      type: "FETCH_SHELF"
+    })
+
+  }, [])
+
+
   return (
     <div className="container">
       <h2>Shelf</h2>
       <p>All of the available items can be seen here.</p>
+      <ul>
+        {shelf.map((shelfItem) => (
+          <li key={shelfItem.id}>
+            <img src={shelfItem.image_url} />
+            {shelfItem.description}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
